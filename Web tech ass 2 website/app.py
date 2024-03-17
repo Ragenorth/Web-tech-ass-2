034605dec8007289b08b6cb199215ab3e7cc3a37
Billy Chai <bcs22020031@student.uts.edu.my>
2024-03-17 11:22:08 +0800

    Update index.html to latest version

--- Web tech ass 2 website/app.py
+++ Web tech ass 2 website/app.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('123.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['name']
     email = request.form['email']
+    phone = request.form['phone']
+    department = request.form['department']
+    address = request.form['address']
+    dob = request.form['dob']
+    gender = request.form['gender']
     comment = request.form['comment']
 
     with open('feedback.txt', 'a') as file:
-        file.write(f"Name: {name}\nEmail: {email}\nComment: {comment}\n\n")
+        file.write(f"Name: {name}\nEmail: {email}\nPhone:{phone}\nDepartment:{department}\nAddress:{address}\nDate of birth: {dob}\nGender:{gender}\nComment: {comment}\n\n")
 
-    return 'Feedback submitted successfully!'
+    return redirect(url_for('submitted'))
+
+@app.route('/submitted')
+def submitted():
+    return render_template('submitted.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)
